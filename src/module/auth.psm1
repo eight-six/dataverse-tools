3e05eaa4838eba8358f09dab52c9855077d7cc9b
Steven Rose <steven@eightsix.io>
2024-01-20 13:54:25 +0000

    WIP

--- src/module/auth.psm1
+++ src/module/auth.psm1
@@ -1,21 +1,30 @@
 
 $Script:AccessToken = $null
 
-function Get-AuthToken{
+function Get-AuthToken {
     param(
         [Parameter(Mandatory = $true, Position = 0)]
         [ValidateNotNullOrWhiteSpace()]
-        [string]$ResourceUrl
+        [string]$ResourceUrl,
+
+        [switch]$ForceRefresh,
+
+        [switch]$NoCache
     )
 
-    if($null -eq $Script:AccessToken -or $Script:AccessToken.ExpiresOn -lt (Get-Date)){
+    $AccessToken = $null
+
+    if ($ForceRefresh.IsPresent -or $null -eq $Script:AccessToken -or $Script:AccessToken.ExpiresOn -lt (Get-Date)) {
         Write-Verbose "Getting new access token for $ResourceUrl" -Verbose
-        $Script:AccessToken = Get-AzAccessToken -ResourceUrl $ResourceUrl
-    } else {
+        $AccessToken = Get-AzAccessToken -ResourceUrl $ResourceUrl
+    }
+    else {
         Write-Verbose "Using cached access token for $ResourceUrl" -Verbose
     }
 
-    $Script:AccessToken.Token
+    $Script:AccessToken = $NoCache.IsPresent ? $null : $AccessToken
+
+    $AccessToken.Token
 }
 
 
